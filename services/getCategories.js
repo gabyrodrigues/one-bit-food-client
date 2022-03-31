b89dee1c482f8afdc9cd66862a5c3960ce26d295
gabyrodrigues <gabyrlima.99@gmail.com>
2022-03-30 21:44:37 -0300

    getting categories from api and filtering category on click

---
+++ services/getCategories.js
@@ -0,0 +1,17 @@
+import useSWR from "swr"
+
+export default function getCategories() {
+	const fetcher = (...args) => fetch(...args).then((response) => response.json())
+
+	const { data, error } = useSWR(
+		`${process.env.apiUrl}/api/categories`,
+		fetcher,
+		{ revalidateOnFocus: false }
+	)
+
+	return {
+		categories: data,
+		isLoading: !error && !data,
+		isError: error
+	}
+}
